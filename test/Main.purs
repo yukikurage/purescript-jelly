b6d063e6bb5c5c6e3050ac2f7833ef440f385cd4
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-05-24 15:06:50 +0900

    use as import

--- test/Main.purs
+++ test/Main.purs
@@ -6,21 +6,21 @@ import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Jelly.Data.Jelly (newJelly)
 import Jelly.Data.Props (on)
-import Jelly.HTML (el, text)
+import Jelly.HTML as H
 import Jelly.RunApp (runApp)
 
 main :: Effect Unit
 main = runApp do
   counterValue /\ modifyCounterValue <- newJelly 0
 
-  el "div" []
-    [ el "button"
+  H.div []
+    [ H.button
         [ on "click" \_ -> modifyCounterValue (_ + 1)
         ]
-        [ text $ pure "+" ]
-    , el "div" [] $ [ text $ show <$> counterValue ]
-    , el "button"
+        [ H.text $ pure "+" ]
+    , H.div [] [ H.text $ show <$> counterValue ]
+    , H.button
         [ on "click" \_ -> modifyCounterValue (_ - 1)
         ]
-        [ text $ pure "-" ]
+        [ H.text $ pure "-" ]
     ]
