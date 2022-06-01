99493722e4f3c200cfda861759989400eeea02fb
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-06-01 20:41:48 +0900

    Revert "Revert "replace system to Monad Base""
    This reverts commit 7af8c613e648a8b14dc38e9407292ec44e6bd923.

--- src/Jelly/Hooks/UseJelly.purs
+++ src/Jelly/Hooks/UseJelly.purs
@@ -2,8 +2,9 @@ module Jelly.Hooks.UseJelly where
 
 import Prelude
 
-import Jelly.Data.Hooks (Hooks, liftJelly)
+import Jelly.Data.Hooks (Hooks)
 import Jelly.Data.Jelly (Jelly, launchJelly_)
+import Jelly.Data.Jelly.Class (liftJelly)
 
 useJelly :: forall r. Jelly Unit -> Hooks r Unit
 useJelly x = liftJelly $ launchJelly_ x
