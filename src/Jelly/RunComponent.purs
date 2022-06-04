80566641f0373fc1f7c8227795069a6887b88346
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-06-04 15:16:09 +0900

    fix RunComponent bug

--- src/Jelly/RunComponent.purs
+++ src/Jelly/RunComponent.purs
@@ -4,9 +4,10 @@ import Prelude
 
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Class (liftEffect)
 import Jelly.Data.Hooks (runHooks)
-import Jelly.Data.Jelly (launchJelly_)
-import Jelly.HTML (Component, addChildrenJelly)
+import Jelly.Data.Jelly (alone, launchJelly_)
+import Jelly.HTML (Component, updateChildren)
 import Web.HTML (window)
 import Web.HTML.HTMLDocument (body)
 import Web.HTML.HTMLElement (toNode)
@@ -17,8 +18,10 @@ runComponent :: forall r. r -> Component r -> Effect Unit
 runComponent r component = do
   bodyMaybe <- body =<< document =<< window
 
-  { nodesJelly } <- runHooks r component
+  let nodesJelly = runHooks r component
 
   case bodyMaybe of
-    Just b -> launchJelly_ $ addChildrenJelly (toNode b) nodesJelly
+    Just b -> alone $ launchJelly_ do
+      nodes <- nodesJelly
+      liftEffect $ updateChildren (toNode b) nodes
     Nothing -> pure unit
