c7a85d12c83cdf613aa4c3a3e9d5f2b6fda9994d
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-07-27 15:25:16 +0900

    add memo

--- src/Jelly/Data/Signal.js
+++ src/Jelly/Data/Signal.js
@@ -1,121 +1,103 @@
 "use strict";
 
 /*
-Signal は同期関数を用いて、ある変数の操作を関連する変数に広げる
-parentSignal は、値が更新された時に childSignal に通知をする
+Signal は時間変動する変数。変更された時に Observer に通知を送る。
 */
 
-/**
- * update は Signal を受け取って、a を返す Effect
- *  受け取った Signal に依存を追加する
- * eq は同値判定に使う関数
- *  同時に初期化も行う
- */
-export const newSignalImpl =
-  (update /* :: GenericSignal -> Effect a */) =>
-  (eq /* :: a -> a -> Boolean */) =>
-  () => {
+export const newSignalImpl = (value /* a */) => () => ({
+  observers: new Set(),
+  value,
+});
+
+export const newObserverImpl =
+  (effect /* :: Observer -> Effect (Effect Unit) */) => () => {
     // 初期化
-    const signal = {
-      parentSignals: new Set(),
-      childSignals: new Set(),
-      eq,
+    const observer = {
+      signals: new Set(),
     };
 
-    signal.update = update(signal);
+    observer.effect = effect(observer);
 
-    // 値および依存関係の更新
-    signal.value = signal.update();
+    // 依存関係、callback の更新
+    observer.callback = observer.effect();
 
-    return signal;
-  };
-
-export const newRefImpl =
-  (tuple) => (initValue) => (eq /* :: a -> a -> Boolean */) => () => {
-    let value = initValue;
-
-    const ref = {
-      childSignals: new Set(),
-    };
-
-    const readRef = (childSignal /* :: GenericSignal */) => () => {
-      connect(ref, childSignal);
-      return value;
-    };
-
-    const writeRef = (newValue /* :: a */) => () => {
-      // 前の値を保存しておく
-      const oldValue = value;
-      value = newValue;
-
-      if (!eq(oldValue)(value)) {
-        // 値が変化した場合、子の Signal を update する
-        [...ref.childSignals].map((childSignal) => {
-          updateSignalImpl(childSignal)();
-        });
-      }
-    };
-
-    return tuple(readRef)(writeRef);
+    return observer;
   };
 
 /**
- * Signal の value を読む時に依存関係を追加する
- */
-export const readSignalImpl =
-  (childSignal /* :: GenericSignal */) => (signal /* :: Signal a */) => () => {
-    connect(signal, childSignal);
-    return signal.value;
-  };
-
-export const readSignalAloneImpl = (signal /* :: Signal a */) => () => {
-  return signal.value;
-};
-
-/**
- * Signal の Value を更新する
- * 葉の Signal や、外部の変数に依存する処理がある場合、
- * また、親の Signal が更新されたときに呼び出される
- */
-export const updateSignalImpl = (signal /* :: Signal a */) => () => {
-  // 依存関係を一度リセットする
-  signal.parentSignals.map((parentSignal) => {
-    disconnect(parentSignal, signal);
-  });
-
-  // 前の値を保存しておく
-  const oldValue = signal.value;
-
-  // 値および依存関係の更新
-  signal.value = signal.update();
-
-  if (!signal.eq(oldValue)(signal.value)) {
-    // 値が変化した場合、子の Signal を update する
-    [...signal.childSignals].map((childSignal) => {
-      updateSignalImpl(childSignal)();
-    });
-  }
-};
-
-/**
- * Signal を親 Signal から切り離す
+ * Observer を親 Observer から切り離す
  * つまり、更新されなくなる
  * update によって再接続が可能
  */
-export const disconnectSignalImpl = (signal /* :: Signal a */) => () => {
-  signal.parentSignals.map((parentSignal) => {
-    disconnect(parentSignal, signal);
+export const disconnectObserverImpl = (observer /* :: Observer */) => () => {
+  observer.signals.map((signal) => {
+    disconnect(observer, signal);
   });
 };
 
 // Signal を接続
-const connect = (parentSignal, childSignal) => {
-  parentSignal.childSignals.add(childSignal);
-  childSignal.parentSignals.add(parentSignal);
+const connect = (observer, signal) => {
+  observer.signals.add(signal);
+  signal.observers.add(observer);
 };
 
 // Signal を切断
-const disconnect = (parentSignal, childSignal) => {
-  parentSignal.childSignals.delete(childSignal);
-  childSignal.parentSignals.delete(parentSignal);
+const disconnect = (observer, signal) => {
+  observer.signals.delete(signal);
+  signal.observers.delete(observer);
 };
+
+export const readSignalImpl =
+  (observer /* :: Observer */) => (signal /* :: Signal a */) => () => {
+    connect(observer, signal);
+    return signal.value;
+  };
+
+export const modifySignalImpl =
+  (eq /* :: a -> a -> Boolean */) =>
+  (signal /* :: Signal a */) =>
+  (modifyFunction /* :: a -> a */) => {
+    const oldValue = signal.value;
+
+    const newValue = modifyFunction(oldValue);
+
+    const oldObservers = [...signal.observers];
+
+    if (!eq(oldValue)(newValue)) {
+      // 値が変化した場合
+      // callback を呼び出す
+      oldObservers.map((observer) => {
+        observer.callback();
+      });
+      // 値を書き換える
+      signal.value = newValue;
+      oldObservers.map((observer) => {
+        // 古い依存関係を破棄する
+        disconnectObserverImpl(observer)();
+        // 依存関係と callback を更新する
+        observer.callback = observer.effect();
+      });
+    }
+  };
+
+export const memoImpl =
+  (eq /* :: a -> a -> Boolean */) =>
+  (signalM /* :: Observer -> Effect {value :: a, callback :: Effect Unit }*/) =>
+  () => {
+    let state = undefined;
+
+    const effect = (obs) => {
+      const { value, callback } = signalM(obs)();
+      if(state === undefined) {
+        state = {
+          observers: new Set(),
+          value,
+        }
+      } else {
+        modifySignalImpl(eq)(state)(() => value);
+      }
+      return callback;
+    }
+
+    return newObserverImpl(effect)();
+  };
--- src/Jelly/Data/Signal.purs
+++ src/Jelly/Data/Signal.purs
@@ -4,104 +4,87 @@ import Prelude
 
 import Control.Monad.Reader (class MonadAsk, class MonadReader, ReaderT, ask, runReaderT)
 import Control.Monad.Rec.Class (class MonadRec)
+import Control.Monad.Writer (class MonadTell, class MonadWriter, WriterT, runWriterT)
+import Control.Safely (for_)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Class (class MonadEffect, liftEffect)
-import Unsafe.Coerce (unsafeCoerce)
 
--- | Signal は動的に変化するような値を表す
--- | 内部に依存先と依存されている Signal を保存されていて、 Signal が更新したら、依存されている Signal も更新する
 foreign import data Signal :: Type -> Type
 
--- | 型情報を忘れた Signal
--- | SignalM で使う
--- | 依存関係の解決にしか興味がない
-foreign import data GenericSignal :: Type
-
-toGenericSignal :: forall a. Signal a -> GenericSignal
-toGenericSignal = unsafeCoerce
+foreign import data Observer :: Type
 
 foreign import newSignalImpl
   :: forall a
-   . (GenericSignal -> Effect a)
-  -> (a -> a -> Boolean)
+   . a
   -> Effect (Signal a)
 
-foreign import newRefImpl
+foreign import newObserverImpl
   :: forall a
-   . ( (GenericSignal -> Effect a)
-       -> (a -> Effect Unit)
-       -> Tuple (GenericSignal -> Effect a) (a -> Effect Unit)
-     )
-  -> a
-  -> (a -> a -> Boolean)
-  -> Tuple (GenericSignal -> Effect a) (a -> Effect Unit)
+   . (Observer -> Effect a)
+  -> Effect Observer
 
-foreign import readSignalImpl :: forall a. GenericSignal -> Signal a -> Effect a
+foreign import disconnectObserverImpl :: Observer -> Effect Unit
 
-foreign import readSignalAloneImpl
-  :: forall a. Signal a -> Effect a
+foreign import readSignalImpl
+  :: forall a. Observer -> Signal a -> Effect a
 
-foreign import updateSignalImpl :: forall a. Signal a -> Effect Unit
-
-foreign import disconnectSignalImpl :: forall a. Signal a -> Effect Unit
+foreign import modifySignalImpl
+  :: forall a. (a -> a -> Boolean) -> Signal a -> (a -> a) -> Effect Unit
 
 -- | SignalM モナドは Signal の値を更新するためのモナド
 -- | 基本的には Effect だが、Reader モナドによりいつでも更新対象の Signal をとってくることができる
 -- | したがって、動的な依存関係の自動解決が可能となっている
-newtype SignalM a = SignalM (ReaderT GenericSignal Effect a)
+newtype SignalM a = SignalM
+  (ReaderT Observer (WriterT (Array (Effect Unit)) Effect) a)
 
 derive newtype instance Functor SignalM
 derive newtype instance Applicative SignalM
 derive newtype instance Apply SignalM
 derive newtype instance Bind SignalM
 derive newtype instance Monad SignalM
-derive newtype instance MonadAsk GenericSignal SignalM
-derive newtype instance MonadReader GenericSignal SignalM
-derive newtype instance MonadEffect SignalM
+derive newtype instance MonadAsk Observer SignalM
+derive newtype instance MonadReader Observer SignalM
 derive newtype instance MonadRec SignalM
+derive newtype instance MonadTell (Array (Effect Unit)) SignalM
+derive newtype instance MonadWriter (Array (Effect Unit)) SignalM
+derive newtype instance MonadEffect SignalM
 
-runSignalM :: forall a. SignalM a -> GenericSignal -> Effect a
-runSignalM (SignalM m) = runReaderT m
+runSignalM
+  :: forall a. SignalM a -> Observer -> Effect (Tuple a (Effect Unit))
+runSignalM (SignalM m) obs = do
+  Tuple a callbacks <- runWriterT (runReaderT m obs)
+  pure $ Tuple a do
+    for_ callbacks identity
 
-newSignal :: forall a. Eq a => SignalM a -> Effect (Signal a)
-newSignal update = newSignalImpl (runSignalM update) eq
+newSignal :: forall a. a -> Effect (Signal a)
+newSignal = newSignalImpl
 
-newSignalWithEq
-  :: forall a. (a -> a -> Boolean) -> SignalM a -> Effect (Signal a)
-newSignalWithEq eqFunction update = newSignalImpl (runSignalM update) eqFunction
-
-newSignalWithoutEq :: forall a. SignalM a -> Effect (Signal a)
-newSignalWithoutEq update = newSignalImpl (runSignalM update) \_ _ -> false
-
-newRef :: forall a. Eq a => a -> Tuple (SignalM a) (a -> Effect Unit)
-newRef a = newRefWithEq eq a
-
-newRefWithEq
-  :: forall a. (a -> a -> Boolean) -> a -> Tuple (SignalM a) (a -> Effect Unit)
-newRefWithEq eqFunction a =
+launchSignalM :: SignalM Unit -> Effect Observer
+launchSignalM signalM = do
   let
-    Tuple readRef writeRef = newRefImpl Tuple a eqFunction
-    readRefRes = do
-      genericSignal <- ask
-      liftEffect $ readRef genericSignal
+    effect = \observer -> do
+      Tuple _ callback <- runSignalM signalM observer
+      pure callback
+  newObserverImpl effect
 
-  in
-    Tuple readRefRes writeRef
+disconnectObserver :: Observer -> Effect Unit
+disconnectObserver = disconnectObserverImpl
 
-newRefWithoutEq :: forall a. a -> Tuple (SignalM a) (a -> Effect Unit)
-newRefWithoutEq = newRefWithEq \_ _ -> false
+modifySignal
+  :: forall a. Eq a => Signal a -> (a -> a) -> Effect Unit
+modifySignal signal f = modifySignalImpl eq signal f
+
+modifySignalForce :: forall a. Signal a -> (a -> a) -> Effect Unit
+modifySignalForce signal f = modifySignalImpl (\_ _ -> false) signal f
+
+writeSignal :: forall a. Eq a => Signal a -> a -> Effect Unit
+writeSignal signal a = modifySignal signal (const a)
+
+writeSignalForce :: forall a. Signal a -> a -> Effect Unit
+writeSignalForce signal a = modifySignalForce signal (const a)
 
 readSignal :: forall a. Signal a -> SignalM a
 readSignal signal = do
-  genericSignal <- ask
-  liftEffect $ readSignalImpl genericSignal signal
-
-readSignalAlone :: forall a. Signal a -> Effect a
-readSignalAlone = readSignalAloneImpl
-
-updateSignal :: forall a. Signal a -> Effect Unit
-updateSignal = updateSignalImpl
-
-disconnectSignal :: forall a. Signal a -> Effect Unit
-disconnectSignal = disconnectSignalImpl
+  observer <- ask
+  liftEffect $ readSignalImpl observer signal
