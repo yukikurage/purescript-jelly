ece90b893694ab674c19e55ee0c997d8fadb2102
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-09-25 00:00:21 +0900

    add link component

---
+++ src/Jelly/Router/Components.purs
@@ -0,0 +1,30 @@
+module Jelly.Router.Components where
+
+import Prelude
+
+import Jelly.Core.Data.Component (Component, el)
+import Jelly.Core.Data.Hooks (hooks)
+import Jelly.Core.Data.Prop (Prop, on)
+import Jelly.Router.Data.Router (RouterContext, useRouter)
+import Jelly.Router.Data.Url (Url)
+import Web.Event.Event (preventDefault)
+import Web.HTML.Event.EventTypes (click)
+
+link
+  :: forall context
+   . Url
+  -> Array Prop
+  -> Component (RouterContext context)
+  -> Component (RouterContext context)
+link url props component = hooks do
+  { pushUrl } <- useRouter
+  let
+    handleClick e = do
+      preventDefault e
+      pushUrl url
+
+  pure $ el "a" (props <> [ on click handleClick ]) component
+
+link_
+  :: forall context. Url -> Component (RouterContext context) -> Component (RouterContext context)
+link_ url component = link url [] component
