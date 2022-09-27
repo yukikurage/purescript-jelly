00d0074aee71b3b210313bf0f2eb27546f24402c
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-09-28 01:17:35 +0900

    fix name of useEffectSignal

--- src/Jelly/Core/Hooks/UseEffectSignal.purs
+++ src/Jelly/Core/Hooks/UseEffectSignal.purs
@@ -9,8 +9,8 @@ import Jelly.Core.Data.Hooks (Hooks)
 import Jelly.Core.Data.Signal (Signal, get, send, signal)
 import Jelly.Core.Hooks.UseSignal (useSignal)
 
-useAffSignal :: forall context a. Signal (Effect a) -> Hooks context (Signal a)
-useAffSignal sig = do
+useEffectSignal :: forall context a. Signal (Effect a) -> Hooks context (Signal a)
+useEffectSignal sig = do
   vSig /\ vAtom <- liftEffect $ signal =<< join (get sig)
   useSignal $ sig <#> \eff -> do
     send vAtom =<< eff
